475691e5e564d62ba3db4c732ca09551c3e378c8
Logan Patino <loganpatino10@gmail.com>
2018-06-26 11:06:08 -0400

    refactor(main): Add temp variable

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,6 @@
 public class MainClass {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String helloString = "Hello world!";
+    System.out.println(helloString);
   }
 }
