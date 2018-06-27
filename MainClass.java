3f7bcbf3aac922c52349f6aa820cc141ab62666f
Logan Patino <loganpatino10@gmail.com>
2018-06-27 10:42:19 -0400

    refactor(java): Remove print function
    BREAKING CHANGE: print function was removed

--- MainClass.java
+++ MainClass.java
@@ -1,13 +1,6 @@
 public class MainClass {
   public static void main(String[] args) {
-    print("Hello world");
-    print("plz");
-  }
-
-  private static void print(String str) {
-    System.out.println(str);
-    System.out.println(str);
-    System.out.println(str);
+    System.out.println("Hello world");
   }
 }
 
