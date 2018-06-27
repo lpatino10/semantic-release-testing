a3a8249960ae7801c695cc2ef679927727a9a43a
Logan Patino <loganpatino10@gmail.com>
2018-06-27 09:42:35 -0400

    More breaking stuff (#4)
    * feat(java): Add new method
    
    * docs(README): Give some closing thoughts
    
    BREAKING CHANGES:
    - we broke this thing
    - we broke another
    - sorry

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,10 @@
 public class MainClass {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    print("Hello world!");
+  }
+
+  private static void print(String str) {
+    System.out.println(str);
   }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # `semantic-release` testing
 This is just a repo to mess around with `semantic-release` to be used for the Watson SDKs. Shhhhh :eyes::eyes::eyes:
+
+I think we're almost done here.
