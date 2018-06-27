4f602a9231feb369b113d7c186494171d2b560f9
Logan Patino <loganpatino10@gmail.com>
2018-06-27 10:30:04 -0400

    Squash and break (#5)
    * feat: Add some more printss
    
    * feat: Add EVEN MORE prints
    
    * refactor: Be a bit more serious
    
    BREAKING CHANGE: That one feature is broken. We did this because we have to.
    
    To fix said breaking change, migrate to the new stuff.

--- MainClass.java
+++ MainClass.java
@@ -1,9 +1,12 @@
 public class MainClass {
   public static void main(String[] args) {
-    print("Hello world!");
+    print("Hello world");
+    print("plz");
   }
 
   private static void print(String str) {
+    System.out.println(str);
+    System.out.println(str);
     System.out.println(str);
   }
 }
